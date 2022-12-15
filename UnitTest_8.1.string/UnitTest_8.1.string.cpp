827d042896c442136d76ec2019b5ea97a5bc55a2
viksi01 <victoria.pyatnicka@gmail.com>
2022-12-16 01:34:56 +0200

    commit1_string

---
+++ UnitTest_8.1.string/UnitTest_8.1.string.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8.1.string/lab_8.1.string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81string
+{
+	TEST_CLASS(UnitTest81string)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string str{ "aaboocba" };
+			int t = Count(str);
+			Assert::AreEqual(t, 2);
+
+		}
+	};
+}
---
+++ lab_8.1.string/lab_8.1.string.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Count(const string s)
+{
+	int k = 0;
+	size_t pos = 0;
+	while ((pos = s.find('a', pos)) != -1)
+	{
+		pos++;
+		if (((s[pos - 1] == 'a' || s[pos - 1] == 'b' || s[pos - 1] == 'c')) && ((s[pos] == 'a' || s[pos] == 'b' || s[pos] == 'c'))
+			&& ((s[pos + 1] == 'a' || s[pos + 1] == 'b' || s[pos + 1] == 'c')))
+			k++;
+	}
+	pos = 0;
+
+	while ((pos = s.find('b', pos)) != -1)
+
+	{
+		pos ++;
+		if (((s[pos - 1] == 'a' || s[pos - 1] == 'b' || s[pos - 1] == 'c')) && ((s[pos] == 'a' || s[pos] == 'b' || s[pos] == 'c'))
+			&& ((s[pos + 1] == 'a' || s[pos + 1] == 'b' || s[pos + 1] == 'c')))
+			k++;
+	}
+
+	pos = 0;
+
+	while ((pos = s.find('c', pos)) != -1)
+
+	{
+		pos++;
+		if (((s[pos - 1] == 'a' || s[pos - 1] == 'b' || s[pos - 1] == 'c')) && ((s[pos] == 'a' || s[pos] == 'b' || s[pos] == 'c'))
+			&& ((s[pos + 1] == 'a' || s[pos + 1] == 'b' || s[pos + 1] == 'c')))
+			k++;
+	}
+
+	return k;
+}
+string Change(string& s)
+{
+	size_t pos = 0;
+	while ((pos = s.find('a', pos)) != -1)
+	{
+		if ((s[pos + 1] == 'a' || s[pos + 1] == 'b' || s[pos + 1] == 'c'))
+			if ((s[pos + 2] == 'a' || s[pos + 2] == 'b' || s[pos + 2] == 'c'))
+				s.replace(pos, 3, "**");
+
+		while ((pos = s.find('b', pos)) != -1)
+		{
+			if ((s[pos + 1] == 'a' || s[pos + 1] == 'b' || s[pos + 1] == 'c'))
+				if ((s[pos + 2] == 'a' || s[pos + 2] == 'b' || s[pos + 2] == 'c'))
+					s.replace(pos, 3, "**");
+
+			while ((pos = s.find('c', pos)) != -1)
+			{
+				if ((s[pos + 1] == 'a' || s[pos + 1] == 'b' || s[pos + 1] == 'c'))
+					if ((s[pos + 2] == 'a' || s[pos + 2] == 'b' || s[pos + 2] == 'c'))
+						s.replace(pos, 3, "**");
+			}
+		}
+
+	}
+	return s;
+}
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "String contained " << Count(str) << " groups of 'a', 'b', 'c'" << endl;
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	
+		return 0;
+}
